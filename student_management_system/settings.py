054794b17a161928e0cd92804b554f832f251232
Ashish <vajpayee.ashish8@gmail.com>
2022-06-21 17:49:50 +0530

    initial commit

--- student_management_system/settings.py
+++ student_management_system/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 import dj_database_url
 import os
+import django_heroku
 from pathlib import Path
 
 
@@ -167,3 +168,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 prod_db = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
+
+
+#Activate Django-Heroku
+django_heroku.settings(locals())
